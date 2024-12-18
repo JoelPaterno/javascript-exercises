cc479f9d8cf724de9b080bc719d4af33637f7b30
JoelPaterno <joelpaterno1@gmail.com>
2024-12-18 18:27:35 +1100

    complete exercise 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, repeats) {
+    if (repeats > -1) {
+        result = "";
+        for (let i = 0; i < repeats; i++) {
+            result += text;
+        };
+        return result;
+    };
+    return 'ERROR';
 };
 
 // Do not edit below this line
