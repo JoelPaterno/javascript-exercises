ea0b38facafe384d9b5f4c71666aff111d375874
JoelPaterno <joelpaterno1@gmail.com>
2024-12-19 22:05:51 +1100

    lesson: complete 04 remove from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...remove) {
+    
+    let result = array.filter((element) => {
+        if (remove.includes(element)) {
+            return false;
+        } else {
+            return true;
+        };
+    });
+    return result;
 };
 
+//console.log(removeFromArray([1, 2, 3, 4], 3));
+console.log(removeFromArray([1, 2, 2, 3], 2, 3));
 // Do not edit below this line
 module.exports = removeFromArray;
