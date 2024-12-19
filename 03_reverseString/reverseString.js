255603423aedb2ac5904794156d5f04d5f15f04e
JoelPaterno <joelpaterno1@gmail.com>
2024-12-19 17:16:06 +1100

    lesson: exercise 3 reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    result = "";
+    len = text.length;
+    for (let i = 0; i < len; i++) {
+        result += text[len-1-i];
+    };
+    return result;
 };
 
 // Do not edit below this line
