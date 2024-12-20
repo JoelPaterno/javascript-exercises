0c3e4f7aaab53a6936786a95d540f9942816cce0
JoelPaterno <joelpaterno1@gmail.com>
2024-12-20 19:01:19 +1100

    lesson: complete lesson 06 leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (Number.isInteger(year/100)) {
+        if (year%400 === 0) {
+            return true;
+        } else {
+            return false;
+        } 
+    } else if (year%4 === 0) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+// console.log("1900: ", leapYears(1900));
+// console.log("700: ", leapYears(700));
 
 // Do not edit below this line
 module.exports = leapYears;
