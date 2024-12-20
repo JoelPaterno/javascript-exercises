8c278475e222ea7cf131dcfb5b60bb27f3043dd4
JoelPaterno <joelpaterno1@gmail.com>
2024-12-20 19:19:45 +1100

    lesson: complete lesson 07 temp conversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenhiet) {
+  return Math.round(((fahrenhiet - 32) * (5/9)) * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  return Math.round(((celcius * (9/5)) + 32) * 10) / 10;
 };
 
+// console.log("73.2: ", convertToFahrenheit(73.2), "expected: 163.8");
+// console.log("-10: ", convertToFahrenheit(-10), "expected: 14");
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
