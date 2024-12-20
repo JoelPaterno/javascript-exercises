d6e8983804e9c46d93b657041b39901ffdd58cc6
JoelPaterno <joelpaterno1@gmail.com>
2024-12-20 11:57:51 +1100

    lesson: complete 04 sumAll lesson

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    //check type of input return error for negative, non-integer and non-numbers
+    if (typeof start !== 'number' || !Number.isInteger(start) || start < 0 || typeof end !== 'number' || !Number.isInteger(end) || end < 0) {
+        return "ERROR"
+    };
 
+    //large number first
+    if (start > end) {
+        let hold = start;
+        start = end;
+        end = hold;
+    };
+
+    //loop through range of numbers
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    };
+    return sum;
 };
 
 // Do not edit below this line
