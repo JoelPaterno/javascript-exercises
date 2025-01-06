9d1c00cadca20398ad3dbf7fb719376937a2225c
JoelPaterno <joelpaterno1@gmail.com>
2025-01-06 12:58:00 +1100

    lesson: 8 calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,41 @@
-const add = function() {
-	
+const add = function(first, second) {
+  return first + second;
 };
 
-const subtract = function() {
-	
+const subtract = function(first, second) {
+	return first - second;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else { 
+    return arr.reduce((total, currentItem) => {
+      return total + currentItem;
+    });
+  };
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  if (arr.length === 0) {
+    return 0;
+  } else { 
+    return arr.reduce((total, currentItem) => {
+      return total * currentItem;
+    });
+  };
 };
 
-const power = function() {
-	
+const power = function(first, second) {
+	return first ** second;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result = 1;
+  for (let i = num; i > 0; i--) {
+    result *= i;
+  };
+  return result;
 };
 
 // Do not edit below this line
