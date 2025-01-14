0480b6ed6b21e345ac36636ad81fbb2f78ec7b1a
joelpaterno <joelpaterno1@gmail.com>
2025-01-14 11:41:13 +1100

    lesson: 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let oldestAge = 0;
+    let oldestPerson = "";
+    arr.forEach((e) => {
+        if (e.yearOfDeath - e.yearOfBirth > oldestAge) {
+            oldestAge = (e.yearOfDeath - e.yearOfBirth);
+            oldestPerson = e;
+        };
+    });
+    return oldestPerson;
 };
 
 // Do not edit below this line
